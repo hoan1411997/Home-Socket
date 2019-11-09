eecc90baddfcddc8efec2992693e25d72f99f3b7
hoan1411997 <tn.hoan141@gmail.com>
2019-11-09 16:47:49 +0700

    ss

--- server.js
+++ server.js
@@ -1,20 +1,43 @@
 'use strict';
-
+var bodyParser = require('body-parser');
 const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
-
+const app = express();
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
+let device = 0;
+let user = 0;
+let clients = [];
+const DATABASE = require('./modules/database');
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/report', (req, res, next) => {
+ return res.send({
+    status: 200,
+    data: {
+      clients,
+      device,
+      user
+    }
+  })
+ 
+})
+const server = app
+  .use((req, res) => res.sendFile(INDEX))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
+  clients.push(socket.id)
   console.log('Client connected');
+  console.log(`Socket ${socket.id} connected!`);
+
+  socket.on('connection', function (message) {
+    console.log(message)
+  })
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
