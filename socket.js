7db8dff4a062534c55d7db8c177b48f5b034ce1e
hoan1411997 <tn.hoan141@gmail.com>
2019-11-09 15:53:09 +0700

    s

--- socket.js
+++ socket.js
@@ -1,34 +1,15 @@
 const express = require('express');
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
 const DATABASE = require('./modules/database');
-const PORT = 5678;
-
+const path = require('path');
+const PORT = process.env.PORT_SOCKET || 3000;
 
 let clients = [];
-app.use("/assets", express.static(__dirname + "/public"));
-app.set("views", "./views");
+const server = express()
+    .use((req, res) => res.sendFile(INDEX))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+const io = require('socket.io')(server);
 app.get('/', (req, res, next) => {
     res.send({
         status: 200,
@@ -45,14 +26,6 @@ app.get('/clients', (req, res, next) => {
     next()
 })
 
-function ParseJson(jsondata) {
-    try {
-        return JSON.parse(jsondata)
-    } catch (error) {
-        return null
-    }
-
-}
 function sendTime() {
 
     //Đây là một chuỗi JSON
@@ -66,7 +39,7 @@ function sendTime() {
 }
 
 io.on("connection", async function (socket) {
-  
+
     socket.emit('welcome', {
         message: 'Connected !!!'
     })
@@ -74,12 +47,9 @@ io.on("connection", async function (socket) {
     clients.push(socket.id)
     socket.on('disconnect', function () {
         console.log('disconnected', socket.id)
-       
-    });
-    socket.on('connection', function (message) {
-        console.log(message)
-    })
 
+    });
+   
     //khi lắng nghe được lệnh "atime" với một tham số, và chúng ta đặt tên tham số đó là data. Mình thích thì mình đặt thôi
     socket.on('atime', function (data) {
         sendTime()
@@ -89,6 +59,10 @@ io.on("connection", async function (socket) {
         io.sockets.emit('arduino', { message: 'R0' });
         console.log(data)
     })
+    socket.on('connection', function (message) {
+        console.log(message)
+    })
+
 })
 
 server.listen(PORT);
